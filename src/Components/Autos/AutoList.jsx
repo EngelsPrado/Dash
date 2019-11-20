d13f1fa75418ab5620a2505058bdf218204a164b
engels prado <engelslove09@gmail.com>
2019-11-19 22:52:45 -0600

    bug del carrito solucionado

--- src/Components/Autos/AutoList.jsx
+++ src/Components/Autos/AutoList.jsx
@@ -5,57 +5,10 @@ import {Link} from '@reach/router'
 
 
 const AutoList=({dato,user})=>{
-    const [Boton,setBoton]=useState("fas fa-plus")
+
      console.log(dato)
-     useEffect(()=>{
-
-      if (user){
-        async function getState(){
-
-            let state=await firestore.collection("users").doc(user.uid).collection("carrito").get()
-            
-            state.docs.map(el=>{
-
-               let carro=el.data()   
-           
-               if(dato.uid == carro.idAuto){
-
-                  if(!carro.state){
-                       setBoton('fas fa-plus')
-                  }else 
-                      setBoton('fas fa-minus')
-
-               }
-
-            })
   
-         }
-         getState() 
-      } 
-  
-         
-     })
-   
 
-    
-	const handleClickPlusMinus=()=>{
-
-		if (Boton=='fas fa-minus')
-		  {
-			setBoton('fas fa-plus')  
-            firestore.collection("users").doc(user.uid).collection("carrito").doc(dato.uid).update({
-                state:false
-            })
-		  }
-		else 
-		   {
-			   setBoton('fas fa-minus')
-			   firestore.collection("users").doc(user.uid).collection("carrito").doc(dato.uid).set({
-                   idAuto:dato.uid,
-                   state:true
-               })
-			}
-	}
 
     return (                     
 
@@ -66,7 +19,7 @@ const AutoList=({dato,user})=>{
                     
                     
                     <div class="col-12 box border ">
-                   { user? <i onClick={handleClickPlusMinus} class={Boton}></i>:null}
+                  
                         <div class="box-carimage">
                             <img src={dato.photoURL[0]} alt=""/>
                         </div>
